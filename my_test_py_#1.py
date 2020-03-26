4f8592d3d938796243f083f713fe8f65f57b71e6
panosale <panosale@users.noreply.github.com>
2020-03-27 00:33:45 +0200

    Update my_test_py_#1.py

--- my_test_py_#1.py
+++ my_test_py_#1.py
@@ -1,5 +1,5 @@
-s = input("Give a string - type ""exit"" to stop: ")
+s = input("Give a string (type ""exit"" to stop): ")
 while (s != "exit"):
     print("You typed: ", s)
-    s = input("Give a string - type ""exit"" to stop: ")
+    s = input("Give a string (type ""exit"" to stop): ")
 
